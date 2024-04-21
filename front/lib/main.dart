8610400d190d3ccdf328a2655f9fb943d61c4a7d
DrAtsiSama <dareka@outlook.fr>
2024-04-21 23:07:45 +0200

    thème custom - Appainter

--- front/lib/main.dart
+++ front/lib/main.dart
@@ -4,17 +4,34 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:front/screens/home_screen.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:json_theme/json_theme.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
-void main() {
-  runApp(const App());
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final themeStr = await rootBundle.loadString('assets/theme.json'); // fichier config
+  final themeJson = jsonDecode(themeStr); // fichier en obj
+  final theme = ThemeDecoder.decodeThemeData(themeJson)!; // theme flutter
+
+  runApp(App(
+    theme: theme,
+  ));
 }
 
 class App extends StatelessWidget {
-  const App({super.key});
+  final ThemeData theme;
+  const App(
+      {
+        super.key,
+        required this.theme,
+      });
 
   @override
   Widget build(BuildContext context){
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Kiwi Corporation",
       localizationsDelegates: [
@@ -27,6 +44,7 @@ class App extends StatelessWidget {
         Locale('en', ''),
         Locale('fr', ''),
       ],
+      theme: theme,
       home: HomeScreen(),
     );
   }
--- front/lib/screens/home_screen.dart
+++ front/lib/screens/home_screen.dart
@@ -7,8 +7,15 @@ class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+      ),
       body: Center(
-        child: Text(t(context)!.helloWorld),
+        child: Text(t(context)!.helloWorld, style: Theme.of(context).textTheme.displayLarge,),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: const Icon(Icons.language),
       ),
     );
   }
