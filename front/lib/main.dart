e96d63b0d52aec145d8a24d6f3ab86ca993ef564
DrAtsiSama <dareka@outlook.fr>
2024-05-01 15:41:00 +0200

    Connection Firebase + patch application multidex

--- front/lib/main.dart
+++ front/lib/main.dart
@@ -1,4 +1,11 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_functions/cloud_functions.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:front/screens/home_screen.dart';
@@ -7,26 +14,60 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:json_theme/json_theme.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // Initialisation de Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   final themeStr = await rootBundle.loadString('assets/theme.json'); // fichier config
   final themeJson = jsonDecode(themeStr); // fichier en obj
   final theme = ThemeDecoder.decodeThemeData(themeJson)!; // theme flutter
 
+  // Firebase
+  if(kDebugMode){
+    final String host = Platform.isAndroid ? "10.0.2.2" : "localhost";
+    // Connexion aux émulateurs
+    await FirebaseAuth.instance.useAuthEmulator(host, 9099);
+    FirebaseFirestore.instance.useFirestoreEmulator(host, 8082);
+    FirebaseStorage.instance.useStorageEmulator(host, 9199);
+    FirebaseFunctions.instance.useFunctionsEmulator(host, 5002);
+  }
+  // Evite d'avoir du cash
+  await FirebaseFirestore.instance.terminate();
+  await FirebaseFirestore.instance.clearPersistence();
+
   runApp(App(
     theme: theme,
+    firebaseAuth: FirebaseAuth.instance,
+    firebaseFirestore: FirebaseFirestore.instance,
+    firebaseStorage: FirebaseStorage.instance,
+    firebaseFunctions: FirebaseFunctions.instance,
   ));
 }
 
 class App extends StatelessWidget {
+
   final ThemeData theme;
+  final FirebaseAuth firebaseAuth;
+  final FirebaseFirestore firebaseFirestore;
+  final FirebaseStorage firebaseStorage;
+  final FirebaseFunctions firebaseFunctions;
+
   const App(
       {
         super.key,
         required this.theme,
+        required this.firebaseAuth,
+        required this.firebaseFirestore,
+        required this.firebaseStorage,
+        required this.firebaseFunctions,
       });
 
   @override
