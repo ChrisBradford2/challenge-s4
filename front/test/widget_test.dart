5585289807244bf252e890f0b2291c827d3efd40
Nicolas Barbarisi <nico.bar2012@gmail.com>
2024-05-11 23:14:59 +0200

    feat/flutter : Fix test.

---
+++ front/test/widget_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  // Print the test name
+  testWidgets('Test the main0.dart file', (WidgetTester tester) async {
+    // Print the test name
+    if (kDebugMode) {
+      print("Test the main0.dart file");
+    }
+  });
+}
