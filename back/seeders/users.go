6695cc300d4b7b6c60d3c04c2c5906aa2c3ef4f3
Elodie Jolo <elodie.jolo@gmail.com>
2024-06-09 16:21:19 +0200

    test

--- back/seeders/users.go
+++ back/seeders/users.go
@@ -14,6 +14,7 @@ func SeedUsers(db *gorm.DB) error {
 		{Username: "user1", LastName: "Doe", FirstName: "John", Email: "johndoe@gmail.com", Password: "$2a$14$M0HeLvAkI0xqB0CUUR4tEOAXnsEH8lv4h55aaU2vDZkzuA4zrGmsO", Roles: config.RoleUser},
 		{Username: "user2", LastName: "Smith", FirstName: "Jane", Email: "janesmith@gmail.com", Password: "$2a$14$vVHG/vtq8qYUfxe2CNhDlelk6DGWQHYaPI2v4rW/an5gWiOZQi8LK", Roles: config.RoleUser},
 		{Username: "user3", LastName: "Johnson", FirstName: "Alice", Email: "testOrg@gmail.com", Password: "$2a$14$Qi.XxbNIKjVbLsgEmxAMX.kX5Y.zEUfJVGG1N.HYSB2W1Ol54OzLa", Roles: config.RoleOrganizer},
+		{Username: "user4", LastName: "Elo", FirstName: "Test", Email: "test@test.fr", Password: "$2a$12$h8goAAbVN8NpvEr37BxEbuuxCyZCrojpj89UoK4FekkS3ITQnnqn6", Roles: config.RoleUser},
 		{Username: "admin", LastName: "Admin", FirstName: "admin", Email: "test@gmail.com", Password: "$2a$14$Qi.XxbNIKjVbLsgEmxAMX.kX5Y.zEUfJVGG1N.HYSB2W1Ol54OzLa", Roles: config.RoleAdmin},
 	}
 
